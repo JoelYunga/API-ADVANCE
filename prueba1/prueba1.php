2af0c4d12b0cf3f399e3d548020a39bf86a5311c
Chicho777 <cristian.yunga@hotmail.com>
2024-12-03 16:49:48 -0500

    prueba dos

--- prueba1/prueba1.php
+++ prueba1/prueba1.php
@@ -2,4 +2,18 @@
 echo "Hola mundo";
 $cadena = ["a","b","c","d","e","f","g"];
 print_r($cadena);
+$numeros = [1,2,3,4,5,6,7,8,9,10];
+$total = 0;
+foreach($numeros as $value) {
+    $total+=$value;
+}
+print_r($total);
+$totalPar = 0;
+foreach($numeros as $value) {
+    if($value%2==0) {
+        $$totalPar+=$value;
+    }
+    
+}
+print_r($total);
 ?>
